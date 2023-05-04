9e203bc18077f717f9e3866808aec392382fcd03
Maris Lakss <maris.lakss@gmail.com>
2023-05-04 06:04:28 +0300

    Initial Commit

---
+++ GifSearch/GifSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifSearchApp.swift
+//  GifSearch
+//
+//  Created by Māris Lakšs on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GifSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
